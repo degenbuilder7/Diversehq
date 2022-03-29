5c18a02be1d1f88b7aaf1e9a6f0d4159703a37bf
kamalishere <kamaleinstein1@gmail.com>
2022-03-29 04:34:54 +0000

    tiltme

--- diversehq/pages/tokencr.js
+++ diversehq/pages/tokencr.js
@@ -1,14 +1,24 @@
 import React from 'react';
 import styles from './tokencr.module.css';
 import { useEffect } from 'react';
+import VanillaTilt from 'vanilla-tilt';
 
 const tokencr = () => {
-  useEffect( () => { document.querySelector("body").classList.add("home") } );
+  // useEffect( () => { document.querySelector("body").classList.add("home") } );
+  if(typeof window === 'object'){
+    const element = document.querySelector("#tiltme");
+    VanillaTilt.init((element), {
+    max: 25,
+    speed: 400,
+    glare: true,
+    });
+  }
+  
+	
   return(
-    <div className={styles.container}>
+    <div className={styles.container} id="tiltme">
       <div className={styles.card}>
          <div className={styles.content}>
-           <h3>01</h3>
            <img src="https://bitcoinchaser.com/wp-content/uploads/2019/02/what-is-erc-20-token_800x480-compressor.jpg" alt="token" />
             <input type="text" placeholder="Token name" />
             <input type="text" placeholder="Token symbol" />
