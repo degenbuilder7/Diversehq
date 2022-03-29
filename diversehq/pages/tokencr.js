230a9e0568c292ebf7b131c001360b6eb5867604
kamalishere <kamaleinstein1@gmail.com>
2022-03-29 04:02:54 +0000

    css

--- diversehq/pages/tokencr.js
+++ diversehq/pages/tokencr.js
@@ -1,11 +1,14 @@
-import React from 'react'
+import React from 'react';
+import styles from './tokencr.module.css';
+import { useEffect } from 'react';
 
 const tokencr = () => {
+  useEffect( () => { document.querySelector("body").classList.add("home") } );
   return(
-    <div class="container">
-      <div class="card">
-         <div class="content">
-           <h2>01</h2>
+    <div className={styles.container}>
+      <div className={styles.card}>
+         <div className={styles.content}>
+           <h3>01</h3>
            <img src="https://bitcoinchaser.com/wp-content/uploads/2019/02/what-is-erc-20-token_800x480-compressor.jpg" alt="token" />
             <input type="text" placeholder="Token name" />
             <input type="text" placeholder="Token symbol" />
