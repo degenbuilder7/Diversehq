7ebc2b7a47ab3da92cbbc865032d5566deecc6c4
Kamal Nayan <95926324+legendarykamal@users.noreply.github.com>
2022-03-31 23:50:05 +0530

    Update _app.js

--- diversehq/pages/_app.js
+++ diversehq/pages/_app.js
@@ -2,10 +2,15 @@ import '../styles/globals.css'
 import {WalletProvider} from "../utils/WalletContext" 
 
 function MyApp({ Component, pageProps }) {
-  return( 
-  <WalletProvider>
-    <Component {...pageProps} />
-  </WalletProvider>)
+  return(
+       <Layout>
+        <AnimatePresence exitBeforeEnter>
+    ``````<WalletProvider>
+            <Component {...pageProps} key={router.route} />
+          <WalletProvider>
+        </AnimatePresence>
+      </Layout>
+  )
 }
 
 export default MyApp
